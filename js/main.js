2d5fbc6664a3eac1552bb6db2bae7717cfd1af7c
Luis David Rodriguez Arias <luis.1701721583@ucaldas.edu.co>
2024-07-22 14:03:55 -0500

    ingles - español

--- js/main.js
+++ js/main.js
@@ -1,10 +1,45 @@
 document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('.nav-link');
     const sections = document.querySelectorAll('section');
+    const backToTopBtn = document.querySelector('.back-to-top');
+    const languageToggle = document.getElementById('language-toggle');
+
+    // Función para establecer el idioma
+    function setLanguage(lang) {
+        const elements = document.querySelectorAll('[data-key]');
+        elements.forEach(el => {
+            const key = el.getAttribute('data-key');
+            if (translations[lang][key]) {
+                el.innerHTML = translations[lang][key];
+            }
+        });
+
+        // Enviar mensaje a todos los iframes para actualizar su contenido
+        const iframes = document.querySelectorAll('iframe');
+        iframes.forEach(iframe => {
+            iframe.contentWindow.postMessage({ lang: lang, translations: translations[lang] }, '*');
+        });
+    }
+
+    // Leer el idioma seleccionado del localStorage o establecer por defecto
+    let selectedLanguage = localStorage.getItem('language');
+    if (!selectedLanguage) {
+        selectedLanguage = languageToggle.checked ? 'en' : 'es';
+        localStorage.setItem('language', selectedLanguage);
+    }
+    setLanguage(selectedLanguage);
+    languageToggle.checked = selectedLanguage === 'en';
+
+    // Manejar el cambio de idioma
+    languageToggle.addEventListener('change', function() {
+        const lang = this.checked ? 'en' : 'es';
+        localStorage.setItem('language', lang);
+        setLanguage(lang);
+    });
 
     function setActiveLink(sectionId) {
         links.forEach(link => {
-            link.classList.toggle('active', link.getAttribute('data-section') === sectionId);
+            link.classList.toggle('active', link.getAttribute('href') === `#${sectionId}`);
         });
     }
 
@@ -12,13 +47,13 @@ document.addEventListener('DOMContentLoaded', function() {
     links.forEach(link => {
         link.addEventListener('click', function(event) {
             event.preventDefault();
-            const sectionId = this.getAttribute('data-section');
+            const sectionId = this.getAttribute('href').substring(1);
             document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
             setActiveLink(sectionId);
         });
     });
 
-    // Manejar el scroll y cambiar el enlace activo
+    // Manejar el scroll y cambiar el enlace activo y visibilidad del botón back-to-top
     window.addEventListener('scroll', function() {
         let currentSectionId;
         sections.forEach(section => {
@@ -29,6 +64,20 @@ document.addEventListener('DOMContentLoaded', function() {
         });
         if (currentSectionId) {
             setActiveLink(currentSectionId);
+        }
+
+        // Mostrar u ocultar el botón "back-to-top"
+        if (window.scrollY > window.innerHeight) {
+            backToTopBtn.style.display = 'flex';
+        } else {
+            backToTopBtn.style.display = 'none';
+        }
+    });
+
+    // Escuchar mensajes del iframe
+    window.addEventListener('message', function(event) {
+        if (event.data === 'scrollToContact') {
+            document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
         }
     });
 });
---
+++ js/translations.js
@@ -0,0 +1,81 @@
+const translations = {
+    es: {
+        NavHome: 'Inicio',
+        NavAboutMe: 'Sobre mi',
+        NavSkills: 'Habilidades',
+        NavTraining: 'Formación',
+        NavProjects: 'Proyectos',
+        NavContact: 'Contacto',
+
+        welcome: 'Hola! Soy <span>Luis David Rodriguez Arias</span>',
+        role: 'ESTUDIANTE | Ingenieria de Sistemas y Computación',
+        description: 'Estudiante de Ingenieria de Sistemas y Computacion apasionado por el Desarrollo Web.',
+        downloadCV: 'DESCARGAR CV',
+        contact: 'CONTACTO',
+
+        titleAboutMe: '¿Quién soy?',
+        textAboutMe: "Soy un apasionado de la tecnología, la innovación, la inteligencia artificial, los videojuegos y el deporte. Actualmente, estudio Ingeniería de Sistemas y Computación en la Universidad de Caldas, donde he desarrollado una sólida base en estas áreas. A lo largo de mi formación, he complementado mis estudios con diversos cursos y programas que han potenciado mis habilidades y conocimientos. <br><br> He tenido la oportunidad de participar en el Programa de Desarrollo Web de Alura Latam en colaboración con Oracle, además de otros cursos en línea que han enriquecido mi perfil profesional. Estos cursos no solo me han permitido mejorar mis habilidades técnicas, sino que también me han ayudado a crecer como persona. <br><br> Me preocupo por mi desarrollo personal y he tomado cursos sobre manejo del tiempo y superación de la procrastinación. Aunque aún lucho contra la procrastinación, el manejo eficiente del tiempo se ha convertido en una herramienta clave en mi vida, junto con el trabajo por objetivos y recompensas.<br><br> Además de mi interés por la tecnología, los videojuegos y el deporte ocupan un lugar especial en mi vida. Soy un entusiasta del baloncesto, deporte que practico con frecuencia y en el que participo con el equipo de la universidad en diversas competencias.<br><br> Mi objetivo es seguir creciendo tanto profesional como personalmente, aprovechando cada oportunidad para aprender y mejorar, y contribuir de manera significativa en los campos de la tecnología y la innovación.",
+
+        titleSkills: 'Habilidades',
+
+        titleTraining: 'Formación',
+        titleCareer: 'Ingenieria de Sistemas y Computación',
+        status: 'En curso',
+
+        titleProjects: 'Proyectos',
+        project1: 'Encriptador de texto | ALURA - LATAM',
+        project2: 'Pagina web | Power Basquetball Club',
+        project3: 'Proyecto React | Administracion Flota',
+        textback: 'Visitar',
+        textbackRepo: 'Repositorio',
+
+        titleContact: 'Contacto',
+        paragraph1: 'Puede contactarme directamente a través de mi correo electrónico:',
+        paragraph2: 'O puede completar el siguiente formulario y me pondré en contacto con usted lo antes posible.',
+        placeholderName: 'Nombre',
+        placeholderEmail: 'E-mail',
+        placeholderSubject: 'Asunto',
+        placeholderMessage: 'Mensaje',
+        submitButton: 'Enviar'
+
+    },
+    en: {
+        NavHome: 'Home',
+        NavAboutMe: 'About Me',
+        NavSkills: 'Skills',
+        NavTraining: 'Training',
+        NavProjects: 'Projects',
+        NavContact: 'Contact',
+
+        welcome: 'Hello! I am <span>Luis David Rodriguez Arias</span>',
+        role: 'STUDENT | Systems and Computer Engineering',
+        description: 'Student of Systems and Computer Engineering passionate about Web Development.',
+        downloadCV: 'DOWNLOAD CV',
+        contact: 'CONTACT',
+
+        titleAboutMe: '¿Who I Am?',
+        textAboutMe: "I am passionate about technology, innovation, artificial intelligence, video games, and sports. Currently, I am studying Systems and Computer Engineering at the University of Caldas, where I have developed a solid foundation in these areas. Throughout my training, I have supplemented my studies with various courses and programs that have enhanced my skills and knowledge. <br><br> I have had the opportunity to participate in the Web Development Program of Alura Latam in collaboration with Oracle, as well as other online courses that have enriched my professional profile. These courses have not only allowed me to improve my technical skills but also helped me grow as a person. <br><br> I care about my personal development and have taken courses on time management and overcoming procrastination. Although I still struggle with procrastination, efficient time management has become a key tool in my life, along with working towards goals and rewards. <br><br> Besides my interest in technology, video games, and sports hold a special place in my life. I am a basketball enthusiast, a sport I practice frequently and in which I participate with the university team in various competitions. <br><br> My goal is to continue growing both professionally and personally, taking advantage of every opportunity to learn and improve, and contribute significantly to the fields of technology and innovation.",
+
+        titleSkills: 'Skills',
+
+        titleTraining: 'Training',
+        titleCareer: 'Systems and Computer Engineering',
+        status: 'In progress',
+
+        titleProjects: 'Projects',
+        project1: 'Text Encryptor | ALURA - LATAM',
+        project2: 'Wep Page | Power Basquetball Club',
+        project3: 'Project React | Fleet Management',
+        textback: 'Visit',
+        textbackRepo: 'Repository',
+
+        titleContact: 'Contact',
+        paragraph1: 'You can contact me directly through my email:',
+        paragraph2: 'Or you can fill out the form below and I will get back to you as soon as possible.',
+        placeholderName: 'Name',
+        placeholderEmail: 'E-mail',
+        placeholderSubject: 'Subject',
+        placeholderMessage: 'Message',
+        submitButton: 'Send'
+    }
+};
